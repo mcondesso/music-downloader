3262399ab9f5b32be09f4c1c6561f5338e9c17c6
Miguel Condesso <mcondesso@mail.com>
2024-01-23 01:18:48 +0100

    Small fixups for mypy warnings

--- src/csv_handling.py
+++ src/csv_handling.py
@@ -3,7 +3,7 @@ from csv import DictReader, DictWriter
 from typing import Tuple, List
 
 
-def read_csv(file_path: str) -> Tuple[List[dict], str]:
+def read_csv(file_path: str) -> Tuple[List[dict], List[str]]:
     """This function loads the contents of a CSV file into a list of dicts."""
     if not file_path.endswith(".csv"):
         raise ValueError(f"'{file_path}' is not a CSV file.")
@@ -12,11 +12,15 @@ def read_csv(file_path: str) -> Tuple[List[dict], str]:
     with open(file_path, "r", encoding="utf-8") as file:
         csv_reader = DictReader(file)
 
+        fieldnames = csv_reader.fieldnames
+        if fieldnames is None:
+            raise ValueError(f"'{file_path}' has no column names.")
+
         # Read data into list of dictionaries
         for row in csv_reader:
             data.append(dict(row))
 
-    return data, csv_reader.fieldnames
+    return data, fieldnames # type: ignore[return-value]
 
 
 def write_csv(file_path, data, fieldnames):
