dfdd35fa60120a3344ab6ec33853ca2ef9ab3414
Miguel Condesso <mcondesso@mail.com>
2025-09-30 21:12:58 +0200

    Feature: convert mp4 files into mp3 for better DJ controller compatibility

--- run.sh
+++ run.sh
@@ -12,5 +12,10 @@ input_file="$1"
 # Generate the output file based on the input file
 output_file="${input_file%.csv}_with_ids.csv"
 
+# Get the download directory (same as input file, without .csv)
+download_dir="${input_file%.csv}"
+
 # Run the scripts one after the other
-poetry run python create_db_with_youtube_ids.py "$input_file" && poetry run python download_tracks.py "$output_file"
+poetry run python create_db_with_youtube_ids.py "$input_file" && \
+poetry run python download_tracks.py "$output_file" && \
+poetry run python convert_tracks_to_mp3.py "$download_dir" -d
